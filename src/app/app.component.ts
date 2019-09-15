bdb951dd2178114151af33f8b940802005f18543
Phuc Tran <glup3.tran@gmail.com>
2019-09-15 19:10:17 +0200

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'glup3-anime-list';
+  title = 'Glup3\'s Anime List';
+  pokemon = 3;
 }
